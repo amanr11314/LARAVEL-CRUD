76bee501b23ead0ef02902c2a4f8c2b815434dc4
Aman Raj <amanr11314@gmail.com>
2023-07-07 13:29:19 +0530

    refactor: code cleanup

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 // handle search along with pagination
 Route::get('search', function (Request $request) {
     $query = $request->input("query");
@@ -58,30 +54,8 @@ Route::get('search', function (Request $request) {
         $pagination = $results->appends($pagination_);
     }
     return view('products.index')->with($data);
-    // return view('products.index')->with(compact('results', 'sortOrder', 'query'));
+    
 })->name('products.find');
-
-// define Route with Controller
-// Route::get('/users', [UserController::class, 'index']);
-
-// named route products.index
-// Route::get('/', [ProductController::class, 'index'])->name('products.index');
-
-// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-
-// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-
-// Route::get('products/{id}/edit', [ProductController::class, 'edit']);
-
-// Route::put('products/{id}/update', [ProductController::class, 'update']);
-
-// Route::delete('products/{id}/delete', [ProductController::class, 'destroy']);
-
-// Route::get('products/{id}/show', [ProductController::class, 'show']);
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/', [ProductController::class, 'index'])->middleware(['auth', 'verified'])->name('products.index');
 
@@ -90,7 +64,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 
