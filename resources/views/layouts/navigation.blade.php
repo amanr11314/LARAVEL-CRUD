d462062d13fd5134b8d81fd4cd8e751d19b62c11
Aman Raj <amanr11314@gmail.com>
2023-07-07 20:03:29 +0530

    wip: add notification ui in header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,10 +16,79 @@
                         {{ __('Products') }}
                     </x-nav-link>
                 </div>
+
+
             </div>
 
-            <!-- Settings Dropdown -->
+
+
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                {{-- Notification --}}
+                <div class="flex items-center shrink-0">
+                    {{-- Notification Icon --}}
+                    <button id="dropdownToggleButton" data-dropdown-toggle="dropdownToggle">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 21">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M10 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C17 15.4 17 16 16.462 16H3.538C3 16 3 15.4 3 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 10 3.464ZM1.866 8.832a8.458 8.458 0 0 1 2.252-5.714m14.016 5.714a8.458 8.458 0 0 0-2.252-5.714M6.54 16a3.48 3.48 0 0 0 6.92 0H6.54Z" />
+                        </svg>
+                    </button>
+                    <!-- Notifications in a Dropdown menu -->
+                    <div id="dropdownToggle"
+                        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-72 dark:bg-gray-700 dark:divide-gray-600">
+                        <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="dropdownToggleButton">
+                            <li>
+                                <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                    <label class="relative inline-flex items-center w-full cursor-pointer">
+                                        <input type="checkbox" value="" class="sr-only peer">
+                                        <div
+                                            class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-blue-600">
+                                        </div>
+                                        <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Enable
+                                            notifications</span>
+                                    </label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                    <label class="relative inline-flex items-center w-full cursor-pointer">
+                                        <input type="checkbox" value="" class="sr-only peer">
+                                        <div
+                                            class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-blue-600">
+                                        </div>
+                                        <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Enable
+                                            2FA authentication</span>
+                                    </label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                                    <label class="relative inline-flex items-center w-full cursor-pointer">
+                                        <input type="checkbox" value="" class="sr-only peer">
+                                        <div
+                                            class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-500 peer-checked:bg-blue-600">
+                                        </div>
+                                        <span
+                                            class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Subscribe
+                                            to newsletter</span>
+                                    </label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex items-center justify-center">
+                                    <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none"
+                                        href="#">
+                                        {{ __('Mark all as read') }}
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Settings Dropdown -->
+
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
